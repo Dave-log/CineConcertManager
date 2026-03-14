b176bb3ca9288f68647b0c1b98a8717f9f5b71f7
Dave-log <david.garcia45@hotmail.fr>
2026-03-14 15:16:18 +0100

    Remove VideoSettingsLoader from VideoPlayerService

--- src/main/java/org/davelogapps/cineconcertmanager/service/VideoPlayerService.java
+++ src/main/java/org/davelogapps/cineconcertmanager/service/VideoPlayerService.java
@@ -3,7 +3,6 @@ package org.davelogapps.cineconcertmanager.service;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import org.davelogapps.cineconcertmanager.data.VideoLoader;
-import org.davelogapps.cineconcertmanager.data.VideoSettingsLoader;
 import org.davelogapps.cineconcertmanager.model.VideoFile;
 
 import java.io.File;
@@ -11,17 +10,13 @@ import java.util.List;
 
 public class VideoPlayerService {
     private final VideoLoader videoLoader;
-    private final VideoSettingsLoader videoSettingsLoader;
 
     public VideoPlayerService() {
         this.videoLoader = new VideoLoader();
-        this.videoSettingsLoader = new VideoSettingsLoader();
     }
 
     public List<VideoFile> loadVideos(String directoryPath) {
-        List<VideoFile> videoFiles = videoLoader.loadVideosFromDirectory(directoryPath);
-        videoSettingsLoader.loadVideoSettings(videoFiles);
-        return videoFiles;
+        return videoLoader.loadVideosFromDirectory(directoryPath);
     }
 
     public MediaPlayer loadAndPlayVideo(VideoFile videoFile) {
