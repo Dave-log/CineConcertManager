920bf226c494bb1bc59e5035b6f1967d733208bf
Dave-log <david.garcia45@hotmail.fr>
2026-03-14 15:14:36 +0100

    Add Launcher.java which have main method

---
+++ src/main/java/org/davelogapps/cineconcertmanager/Launcher.java
@@ -0,0 +1,9 @@
+package org.davelogapps.cineconcertmanager;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(App.class, args);
+    }
+}
